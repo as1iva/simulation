cdb6915dcea4ef08ef0d12a4cbb719cc435675dc
Tim Fadeev <1mailforwork1@gmail.com>
2024-08-01 13:39:46 +0300

    feat: implement "Predator" class

---
+++ src/Predator.java
@@ -0,0 +1,14 @@
+public class Predator extends Creature {
+    private static final int STRENGTH = 20;
+
+    public Predator() {
+        entityView = "🐺";
+        health = 100;
+        speed = 3;
+    }
+
+    @Override
+    public void makeMove() {
+        // find herbivore
+    }
+}
