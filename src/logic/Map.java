c479ea14a995b4b4428ff36320f77fe43b5658fa
Tim Fadeev <1mailforwork1@gmail.com>
2024-08-16 17:05:11 +0300

    feat: add setup random entity position

--- src/logic/Map.java
+++ src/logic/Map.java
@@ -4,10 +4,14 @@ import entities.Entity;
 import entities.creatures.Herbivore;
 import entities.creatures.Predator;
 import entities.objects.Grass;
+import entities.objects.Rock;
+import entities.objects.Tree;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class Map {
+    Random random = new Random();
     HashMap<Coordinates, Entity> entities = new HashMap<>();
 
     public void setEntity(Coordinates coordinates, Entity entity) {
@@ -22,7 +26,40 @@ public class Map {
         return !entities.containsKey(coordinates);
     }
 
-    public void setUpRandomEntityPositions() {
-        // make randomizer of positions
+    public void setUpRandomEntityPosition() {
+        Entity[] arrayOfEntities = {new Herbivore(), new Predator(), new Grass(), new Rock(), new Tree()};
+        int i = 0;
+
+        while (true) {
+            int xCoordinate = random.nextInt(10);
+            int yCoordinate = random.nextInt(10);
+
+            if (entities.containsKey(new Coordinates(xCoordinate, yCoordinate))) {
+                continue;
+            }
+
+            if (isEnoughObjectsPlaced(entities, arrayOfEntities, i)) {
+                i++;
+            }
+
+            setEntity(new Coordinates(xCoordinate, yCoordinate), arrayOfEntities[i]);
+
+            if (entities.size() == 16) {
+                break;
+            }
+        }
+    }
+
+    private boolean isEnoughObjectsPlaced(HashMap<Coordinates, Entity> entities, Entity[] arrayOfEntities, int i) {
+        int[] countOfEntities = {4, 2, 5, 2, 3};
+        int count = 0;
+
+        for (Entity entity : entities.values()) {
+            if (entity.equals(arrayOfEntities[i])) {
+                count++;
+            }
+        }
+
+        return count == countOfEntities[i];
     }
 }
