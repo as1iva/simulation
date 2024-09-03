6af6543036beb0784199c2f01b44878f602ce43a
Tim Fadeev <1mailforwork1@gmail.com>
2024-09-03 10:25:24 +0300

    feat: add entry point for simulation threads

---
+++ src/logic/Main.java
@@ -0,0 +1,10 @@
+package logic;
+
+public class Main {
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        simulation.startSimulation();
+
+        simulation.pauseSimulation();
+    }
+}
