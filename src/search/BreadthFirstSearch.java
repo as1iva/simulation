85e263142cb50ab61018285d73bd2cf98f6c3fc0
Tim Fadeev <1mailforwork1@gmail.com>
2024-09-08 15:31:42 +0300

    refactor: improve naming in "BreadthFirstSearch" class

--- src/search/BreadthFirstSearch.java
+++ src/search/BreadthFirstSearch.java
@@ -7,10 +7,10 @@ import map.Map;
 import java.util.*;
 
 public class BreadthFirstSearch {
-    public List<Coordinates> BFS(Map map, Coordinates coordinates, Creature entity) {
+    public List<Coordinates> BFS(Map map, Coordinates coordinates, Creature creature) {
 
         Queue<Coordinates> queue = new LinkedList<>();
-        List<Coordinates> checkedEntities = new ArrayList<>();
+        List<Coordinates> checkedCells = new ArrayList<>();
         HashMap<Coordinates, Coordinates> path = new HashMap<>();
         List<Coordinates> newPath = new ArrayList<>();
 
@@ -22,10 +22,10 @@ public class BreadthFirstSearch {
             int x = coordinates.getX();
             int y = coordinates.getY();
 
-            checkedEntities.add(coordinates);
+            checkedCells.add(coordinates);
 
-            if (entity.canEat(map.getEntity(coordinates))) {
-                return recreatePath(coordinates, checkedEntities, path, newPath);
+            if (creature.canEat(map.getEntity(coordinates))) {
+                return recreatePath(coordinates, checkedCells, path, newPath);
             }
 
             Coordinates[] directions = {
@@ -35,19 +35,19 @@ public class BreadthFirstSearch {
                     new Coordinates(x, y - 1)
             };
 
-            for (Coordinates newCoordinates : directions) {
-                int newX = newCoordinates.getX();
-                int newY = newCoordinates.getY();
+            for (Coordinates nextCoordinates : directions) {
+                int nextX = nextCoordinates.getX();
+                int nextY = nextCoordinates.getY();
 
                 boolean isValidMove = (
-                        areCoordinatesInMapBounds(newX, newY) &&
-                        isCellExplorable(checkedEntities, queue, newCoordinates) &&
-                        canMove(map, newCoordinates, entity)
+                        areCoordinatesInMapBounds(nextX, nextY) &&
+                        isCellExplorable(checkedCells, queue, nextCoordinates) &&
+                        canMove(map, nextCoordinates, creature)
                 );
 
                 if (isValidMove) {
-                    queue.offer(newCoordinates);
-                    path.put(newCoordinates, coordinates);
+                    queue.offer(nextCoordinates);
+                    path.put(nextCoordinates, coordinates);
                 }
             }
         }
@@ -58,18 +58,18 @@ public class BreadthFirstSearch {
         return x < Map.MAP_WIDTH && x >= 0 && y < Map.MAP_HEIGHT && y >= 0;
     }
 
-    private boolean isCellExplorable(List<Coordinates> checkedEntities, Queue<Coordinates> queue, Coordinates coordinates) {
-        return !checkedEntities.contains(coordinates) && !queue.contains(coordinates);
+    private boolean isCellExplorable(List<Coordinates> checkedCells, Queue<Coordinates> queue, Coordinates nextCoordinates) {
+        return !checkedCells.contains(nextCoordinates) && !queue.contains(nextCoordinates);
     }
 
-    private boolean canMove(Map map, Coordinates coordinates, Creature entity) {
-        return entity.canEat(map.getEntity(coordinates)) || map.isCellEmpty(coordinates);
+    private boolean canMove(Map map, Coordinates nextCoordinates, Creature creature) {
+        return creature.canEat(map.getEntity(nextCoordinates)) || map.isCellEmpty(nextCoordinates);
     }
 
-    private List<Coordinates> recreatePath(Coordinates coordinates, List<Coordinates> checkedEntities, HashMap<Coordinates, Coordinates> path, List<Coordinates> newPath) {
+    private List<Coordinates> recreatePath(Coordinates coordinates, List<Coordinates> checkedCells, HashMap<Coordinates, Coordinates> path, List<Coordinates> newPath) {
         newPath.add(coordinates);
 
-        while (!newPath.contains(checkedEntities.get(0))) {
+        while (!newPath.contains(checkedCells.get(0))) {
             for (Coordinates key : path.keySet()) {
                 if (key.equals(coordinates)) {
                     newPath.add(path.get(coordinates));
